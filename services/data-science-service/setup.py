57938ccaceef11fb3d2f3acb4fe0a2834bdb071d
Brian Austin <austin.brian@gmail.com>
2019-08-13 14:12:07 -0400

    Make RDSO module that can be pip installed

---
+++ services/data-science-service/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="rdso",
+    version="1.0",
+    description="Python module for data science services for RDSO submission",
+    author=["Brian Austin", "David Waterman"],
+    author_email="datascienceu@u.group",
+    packages=["rdso"],  # same as name
+)
