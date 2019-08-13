739a335be1d26a210505a18c1b8a489c9e533fbb
davidwaterman <david.waterman@bytecubed.com>
2019-08-13 17:42:13 -0400

    UR0 - david.waterman - Update scrapers for improved resume functionality

--- services/data-science-service/imdb-scraper/parser.py
+++ services/data-science-service/imdb-scraper/parser.py
@@ -34,9 +34,12 @@ if __name__ == "__main__":
         json_dir.mkdir()
 
     file_list = []
-    for x in html_dir.iterdir():
-        if x.is_file():
-            file_list.append(x)
+    for html_file in html_dir.iterdir():
+        if html_file.is_file():
+            json_file = json_dir / html_file.stem
+            json_file = json_file.with_suffix('.json')
+            if not json_file.is_file():
+                file_list.append(html_file)
 
     for readfile in file_list:
         with readfile.open('r') as infile:
@@ -44,11 +47,17 @@ if __name__ == "__main__":
 
     for movie_html in tqdm(scraped_htmls):
         parsed_movie = parse_movie(movie_html)
-        ttid = parsed_movie['url'].split('/')[2]
+        try:
+            ttid = parsed_movie['url'].split('/')[2]
+        except KeyError:
+            continue
 
         # Objective 2 date requirements:
-        release_date = parsed_movie['datePublished']
-        if "2009-06-01" <= release_date <= "2019-06-01":
-            writefile = json_dir / f'{ttid}.json'
-            with writefile.open('w') as outfile:
-                json.dump(parsed_movie, outfile, indent=2)
+        try:
+            release_date = parsed_movie['datePublished']
+            if "2009-06-01" <= release_date <= "2019-06-01":
+                writefile = json_dir / f'{ttid}.json'
+                with writefile.open('w') as outfile:
+                    json.dump(parsed_movie, outfile, indent=2)
+        except KeyError:
+            print("Movie with no release date")
