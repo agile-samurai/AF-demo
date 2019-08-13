1b34cbbcef422fbf8d251f4367dc23c2a5aad87a
Ian Williams <ianwilliams@bytecubed.com>
2019-08-13 09:22:16 -0400

    UR-6 adding pipeline

--- config/service-setup/modules/postgres/main.tf
+++ config/service-setup/modules/postgres/main.tf
@@ -1,13 +1,22 @@
 resource "aws_db_instance" "postgres" {
-  allocated_storage   = 40
-  storage_type        = "gp2"
-  engine              = "postgres"
-  engine_version      = "9.6.9"
-  instance_class      = "db.t2.micro"
-  name                = "${var.db_name}"
-  username            = "${var.db_username}"
-  password            = "${var.db_password}"
-  skip_final_snapshot = true
-
+  allocated_storage    = 40
+  storage_type         = "gp2"
+  engine               = "postgres"
+  engine_version       = "9.6.9"
+  instance_class       = "db.t2.micro"
+  name                 = "${var.db_name}"
+  username             = "${var.db_username}"
+  password             = "${var.db_password}"
+  skip_final_snapshot  = true
+  db_subnet_group_name = "${aws_db_subnet_group.postgres}"
   # parameter_group_name = "postgres-9.6.9"
+}
+
+resource "aws_db_subnet_group" "postgres" {
+  name       = "main"
+  subnet_ids = ["${var.public_subnets}"]
+
+  tags = {
+    Name = "My DB subnet group"
+  }
 }
