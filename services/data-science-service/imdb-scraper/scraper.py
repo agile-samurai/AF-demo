3c5d1a9d15c1b0aa5a1b78d014e9c539c474c804
davidwaterman <david.waterman@bytecubed.com>
2019-08-14 14:27:17 -0400

    UR16 - david.waterman - Refactor location of data directory and add vectorizer code

--- services/data-science-service/imdb-scraper/scraper.py
+++ services/data-science-service/imdb-scraper/scraper.py
@@ -55,13 +55,14 @@ def split_genre(genre):
 
 
 if __name__ == '__main__':
-
-    html_dir = pathlib.Path('data', 'imdb_html')
+    cwd = pathlib.Path('.').resolve()
+    data_dir = cwd.parents[0] / 'data'
+    html_dir = data_dir / 'imdb_html'
     if not html_dir.is_dir():
         html_dir.mkdir()
 
     # Read MovieTweetings data file
-    movie_tweets_file = pathlib.Path('data', 'movies.dat')
+    movie_tweets_file = data_dir / 'movies.dat'
     movie_tweets_data = pd.read_csv(str(movie_tweets_file), sep='::',
                                     names=['imdb_id', 'title', 'genres'],
                                     dtype={'imdb_id': str},
