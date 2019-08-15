91eca4b1ceb1af713adcbaa53a0fc9f795b88bc0
bradculley <bradley.culley@bytecubed.com>
2019-08-15 17:39:44 -0400

    UR-3 - Refactoring around moving style objects to CSS and extracting a Spring service for actor business logic

--- ui/server/src/main/java/group/u/records/web/ActorController.java
+++ ui/server/src/main/java/group/u/records/web/ActorController.java
@@ -1,49 +1,29 @@
 package group.u.records.web;
 
 import group.u.records.models.Actor;
-import group.u.records.repository.ActorRepository;
-import org.elasticsearch.common.unit.Fuzziness;
+import group.u.records.service.ActorService;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.elasticsearch.core.query.NativeSearchQueryBuilder;
-import org.springframework.data.elasticsearch.core.query.SearchQuery;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import static org.elasticsearch.index.query.QueryBuilders.matchQuery;
 import static org.springframework.http.ResponseEntity.ok;
 
 @RestController
 @RequestMapping("/actors")
 public class ActorController {
-    private ActorRepository actorRepository;
-//    private Sort sort = new Sort(Sort.Direction.ASC, "name");
+    private ActorService actorService;
 
-    public ActorController(ActorRepository actorRepository ){
-        this.actorRepository = actorRepository;
+    public ActorController(ActorService actorService ){
+        this.actorService = actorService;
     }
 
     @GetMapping
     public ResponseEntity<Page<Actor>> search(@RequestParam("search") String searchString,
                                               @RequestParam(value = "quantity", required = false, defaultValue = "10") int quantity,
                                               @RequestParam(value = "cursor", required = false, defaultValue="0") int cursor){
-        PageRequest pageRequest = PageRequest.of(cursor, quantity);
-
-        SearchQuery searchQuery = new NativeSearchQueryBuilder()
-                .withQuery(matchQuery("name", searchString)
-                        .fuzziness(Fuzziness.TWO))
-                .withPageable(pageRequest)
-                .build();
-
-        return ok(actorRepository.search(searchQuery));
+        return ok(actorService.getActors(searchString, quantity, cursor));
     }
-
-    @GetMapping("/all")
-    public ResponseEntity<Iterable<Actor>> all(){
-        return ok(actorRepository.findAll());
-    }
-
 }
