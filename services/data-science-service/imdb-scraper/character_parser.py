ba38917a9386b23402ed89cefaa5a4af655318a8
Brian Austin <austin.brian@gmail.com>
2019-08-17 16:00:38 -0400

    UR-8 - @austinbrian - Finished character parser

--- services/data-science-service/imdb-scraper/character_parser.py
+++ services/data-science-service/imdb-scraper/character_parser.py
@@ -15,56 +15,86 @@ def parse_movie(html: str):
     bs = BeautifulSoup(html, "html.parser")
     characters = []
     table = bs.find("table", {"class": "cast_list"})
+    if not table:
+        return characters
     for row in table.find_all("tr")[1:]:
-        photo, name, _, character = row.find_all("td")
-        photo_img = photo.find("img")["src"]
-        actor_id = photo.find("a")["href"]
-        character = character.text.strip("\n ")
-        name = name.text.strip("\n ")
-        cdict = dict(
-            zip(
-                ("photo_img", "actor_id", "name", "character"),
-                (photo_img, actor_id, name, character),
+        try:
+            photo, name, _, character = row.find_all("td")
+            photo_img = photo.find("img")["src"]
+            actor_id = photo.find("a")["href"]
+            character = character.text.strip("\n ").replace("\n", "")
+            name = name.text.strip("\n ")
+            cdict = dict(
+                zip(
+                    ("photo_img", "actor_id", "name", "character"),
+                    (photo_img, actor_id, name, character),
+                )
             )
-        )
-        characters.append(cdict)
-        print(cdict)
-        # print("photo:", photo.text.strip())
-        # print("name:", name.text.strip())
-        # print("character:", character.text.strip())
+            characters.append(cdict)
+        except:
+            pass
+        return characters
 
 
 if __name__ == "__main__":
-    html = []
-    testfile = "/Users/brianaustin/Downloads/tt0069049.html"
-    with open(testfile) as f:
-        html = f.read()
-    parsed_movie = parse_movie(html)
+    # html = []
+    # testfile = "/Users/brianaustin/Downloads/tt0069049.html"
+    # with open(testfile) as f:
+    #     html = f.read()
+    # parsed_movie = parse_movie(html)
 
-    # scraped_htmls = []
-    # cwd = pathlib.Path('.').resolve()
-    # data_dir = cwd.parents[0]/'data'
-    # html_dir = data_dir / 'movies_html'
-    # json_dir = data_dir / 'movies_json'
-    # if not json_dir.is_dir():
-    #     json_dir.mkdir()
-    #
-    # file_list = []
-    # for html_file in html_dir.iterdir():
-    #     if html_file.is_file():
-    #         json_file = json_dir / html_file.stem
-    #         json_file = json_file.with_suffix('.json')
+    scraped_htmls = []
+    cwd = pathlib.Path(".").resolve()
+    data_dir = cwd.parents[0] / "data"
+    html_dir = data_dir / "movies_html"
+    json_dir = data_dir / "characters_json"
+    if not json_dir.is_dir():
+        json_dir.mkdir()
+
+    file_list = []  # list of tuples
+    for html_file in html_dir.iterdir():
+        if html_file.is_file():
+            film_id = html_file.stem
+            json_file = json_dir / html_file.stem
+            json_file = json_file.with_suffix(".json")
+            if not json_file.is_file():
+                file_list.append((film_id, html_file))
+
+    for film_id, readfile in file_list:
+        with readfile.open("r") as infile:
+            scraped_htmls.append((film_id, infile.read()))
+
+    for film_id, movie_html in tqdm(scraped_htmls):
+        parsed_movie = parse_movie(movie_html)
+        if len(parsed_movie) > 0:
+            writefile = json_dir / f"{film_id}.json"
+            with writefile.open("w") as outfile:
+                json.dump(parsed_movie, outfile, indent=2)
+        else:
+            continue
+            # counter +=1
+
+            ####
+
+    #     for html_file in html_dir.iterdir():
+    #         if html_file.is_file():
+    #             json_file = json_dir / html_file.stem
+    #             json_file = json_file.with_suffix(".json")
     #         if not json_file.is_file():
-    #             file_list.append(html_file)
+    #             # file_list.append(html_file)
+    #             with readfile.open("r") as infile:
+    #                 scraped_htmls.append(infile.read())
+    #             parsed_movie = parse_movie(html_file)
+    #             writefile = json_dir / f"{json_file}.json"
+    #             with writefile.open("w") as outfile:
+    #                 json.dump(parsed_movie, outfile, indent=2)
+    #             counter += 1
     #
-    # for readfile in file_list:
-    #     with readfile.open('r') as infile:
-    #         scraped_htmls.append(infile.read())
-    #
-    # for movie_html in tqdm(scraped_htmls):
-    #     parsed_movie = parse_movie(movie_html)
-    #     try:
-    #         ttid = parsed_movie['url'].split('/')[2]
-    #     except KeyError:
-    #         continue
-    #
+    # # for readfile in file_list:
+    # #     with readfile.open("r") as infile:
+    # #         scraped_htmls.append(infile.read())
+    # #
+    # # for movie_html in tqdm(scraped_htmls):
+    # #     writefile = json_dir / f'{json_file}.json'
+    # #     with writefile.open('w') as outfile:
+    # #         json.dump(parsed_movie, outfile, indent=2)
