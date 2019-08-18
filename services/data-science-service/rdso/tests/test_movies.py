11b8a8c271dd6c8161e6692cac6f601698d65d70
Brian Austin <austin.brian@gmail.com>
2019-08-18 17:20:22 -0400

    Add tests for secondary genre

--- services/data-science-service/rdso/tests/test_movies.py
+++ services/data-science-service/rdso/tests/test_movies.py
@@ -1,7 +1,30 @@
-from rdso import movies
+from rdso.movies import secondary_genre, load_movietweetings_df
+from rdso.movies import translate_duration
+import requests
+import pandas as pd
+import pytest
+
+
+def test_secondary_genre():
+    assert secondary_genre("Action|Adventure".split("|")[1:]) == "Adventure"
+    assert secondary_genre("Action".split("|")[1:]) == []
+    assert secondary_genre("Action|Adventure|Comedy|Drama".split("|")[1:]) == [
+        "Adventure",
+        "Comedy",
+        "Drama",
+    ]
+
+
+def test_access_to_movietweetings():
+    res = requests.get(
+        "https://raw.githubusercontent.com/sidooms/MovieTweetings/master/latest/movies.dat"
+    )
+    assert res.status_code == requests.codes.ok
+    mv = load_movietweetings_df()
+    assert type(mv) == pd.DataFrame
 
 
 def test_translate_duration():
-    assert movies.translate_duration("PT2H22M") == 142
-    assert movies.translate_duration("PT2H") == 120
-    assert movies.translate_duration("PT23M") == 23
+    assert translate_duration("PT2H22M") == 142
+    assert translate_duration("PT2H") == 120
+    assert translate_duration("PT23M") == 23
