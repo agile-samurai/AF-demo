bcfa7c544cb8bd9e92a50e1493793d3551b570d3
Carlyle S. Davis <carlyle.davis@bytecubed.com>
2019-08-18 11:13:52 -0400

    UR-2 - @carlyledavis: Merging in changes to build out character profiles.

--- ui/server/src/main/java/group/u/records/datasource/ImdbMovieDetailsDataSource.java
+++ ui/server/src/main/java/group/u/records/datasource/ImdbMovieDetailsDataSource.java
@@ -2,34 +2,43 @@ package group.u.records.datasource;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import group.u.records.models.data.Movie;
+import group.u.records.models.entity.MovieCharacter;
 import group.u.records.models.entity.MovieDetail;
 import group.u.records.people.PersonRegistry;
 import group.u.records.service.MovieDetailsDataSource;
 import group.u.records.service.S3DataService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
+import java.util.List;
 
 import static group.u.records.service.Lineage.IMDB;
+import static java.lang.Enum.valueOf;
 
 @Component
 public class ImdbMovieDetailsDataSource extends MovieDetailsDataSource {
 
     private String bucketName;
     private String folder;
+    private String characterFolder;
     private PersonRegistry personRegistry;
     private ObjectMapper objectMapper;
     private S3DataService dataService;
+    private Logger logger = LoggerFactory.getLogger(ImdbMovieDetailsDataSource.class);
 
     public ImdbMovieDetailsDataSource(@Value("${aws.bucketName}") String bucketName,
                                       @Value("${aws.folder}") String folder,
+                                      @Value("${aws.characterFolder}") String characterFolder,
                                       PersonRegistry personRegistry,
                                       ObjectMapper objectMapper,
                                       S3DataService dataService) {
         super(IMDB);
         this.bucketName = bucketName;
         this.folder = folder;
+        this.characterFolder = characterFolder;
         this.personRegistry = personRegistry;
         this.objectMapper = objectMapper;
         this.dataService = dataService;
@@ -44,8 +53,8 @@ public class ImdbMovieDetailsDataSource extends MovieDetailsDataSource {
             Movie movie = objectMapper.readValue(json, Movie.class);
             movie.enrichModel();
             movieDetail = new MovieDetail(movie, this.getLineage());
-
             movieDetail.getPeople().forEach(p->personRegistry.reconcile(p, new MovieDetail(movie, this.getLineage())));
+            getCharacters(id);
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -53,7 +62,23 @@ public class ImdbMovieDetailsDataSource extends MovieDetailsDataSource {
         return movieDetail;
     }
 
-    private String convertId(String bucketName, String imdb) {
-        return bucketName + "/tt" + imdb + ".json";
+    public List<MovieCharacter> getCharacters(String id ){
+        try {
+            String json = dataService.getFileAsString(this.convertId(characterFolder, id ));
+            logger.debug("Character information being processed:  " + json );
+//            Movie movie = objectMapper.readValue(json, Movie.class);
+//            movie.enrichModel();
+
+//            movieDetail = new MovieDetail(movie, this.getLineage());
+//            movieDetail.getPeople().forEach(p->personRegistry.reconcile(p, new MovieDetail(movie, this.getLineage())));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    private String convertId(String folderName, String imdb) {
+        return folderName + "/tt" + imdb + ".json";
     }
 }
