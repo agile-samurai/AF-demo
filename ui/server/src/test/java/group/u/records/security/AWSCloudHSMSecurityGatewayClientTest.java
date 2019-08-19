7a936eb74a202e1c46523e56520d1c5c1cedcb86
Carlyle S. Davis <carlyle.davis@bytecubed.com>
2019-08-18 21:40:41 -0400

    UR-21 - @carlyledavis - Moving this test to an integration test.

--- ui/server/src/test/java/group/u/records/security/AWSCloudHSMSecurityGatewayClientTest.java
+++ ui/server/src/test/java/group/u/records/security/AWSCloudHSMSecurityGatewayClientTest.java
@@ -1,5 +1,6 @@
 package group.u.records.security;
 
+import org.junit.Ignore;
 import org.junit.Test;
 import org.springframework.web.client.RestTemplate;
 
@@ -11,6 +12,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class AWSCloudHSMSecurityGatewayClientTest {
 
     @Test
+    @Ignore
     public void shouldEncryptData(){
 
         AWSCloudHSMSecurityGatewayClient client = new AWSCloudHSMSecurityGatewayClient(new RestTemplate(), "http://13.52.124.47:8080" );
@@ -25,17 +27,18 @@ public class AWSCloudHSMSecurityGatewayClientTest {
     }
 
     @Test
+    @Ignore
     public void shouldFailBasedOnCurrentBug(){
         AWSCloudHSMSecurityGatewayClient client = new AWSCloudHSMSecurityGatewayClient(new RestTemplate(), "http://13.52.124.47:8080" );
         String content = "this is a random text string";
+        String secondContent = "This is secondContent";
         UUID id = UUID.randomUUID();
         String encrypt = client.encrypt(id, content);
-        String secondEncrypt = client.encrypt(id, content);
+        String secondEncrypt = client.encrypt(id, secondContent);
 
-        assertThat(encrypt).isEqualTo(secondEncrypt);
         System.out.println(encrypt);
         assertThat(encrypt).isNotNull();
-        assertThat(content).isEqualTo(client.decrypt(id, secondEncrypt));
+        assertThat(secondContent).isEqualTo(client.decrypt(id, secondEncrypt));
         assertThat(content).isEqualTo(client.decrypt(id, encrypt));
 
     }
