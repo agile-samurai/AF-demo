0790e2f18febe5f1dd1a582208fb3d538e953ba3
Carlyle S. Davis <carlyle.davis@bytecubed.com>
2019-08-18 20:37:13 -0400

    UR-21 - @carlyledavis:  Adding the ability to add Amazon data sources.

--- ui/server/src/main/java/group/u/records/service/AmazonReviewsDataSource.java
+++ ui/server/src/main/java/group/u/records/service/AmazonReviewsDataSource.java
@@ -1,45 +1,64 @@
 package group.u.records.service;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.javafaker.Faker;
+import group.u.records.datasource.ImdbMovieDetailsDataSource;
 import group.u.records.models.Person;
+import group.u.records.models.entity.AmazonReview;
 import group.u.records.models.entity.MovieDetail;
 import group.u.records.models.entity.Review;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
+import java.io.IOException;
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
 
 import static group.u.records.service.Lineage.AMAZON;
 import static java.util.Arrays.asList;
+import static java.util.stream.Collectors.toList;
 
 @Component
 public class AmazonReviewsDataSource extends MovieDetailsDataSource {
 
+    private Logger logger = LoggerFactory.getLogger(AmazonReviewsDataSource.class);
     private S3DataService dataService;
+    private ObjectMapper objectMapper;
 
-    public AmazonReviewsDataSource(S3DataService dataService) {
+    public AmazonReviewsDataSource(S3DataService dataService, ObjectMapper objectMapper) {
         super(AMAZON);
         this.dataService = dataService;
+        this.objectMapper = objectMapper;
     }
 
     @Override
     public MovieDetail getMovieDetails(String id) {
         List<Review> reviews = new ArrayList();
-        reviews.add(new Review( "fake reviewer", LocalDate.now(), Faker.instance().lorem().paragraph()));
+        try {
+            String fileAsString = dataService.getFileAsString("data/reviews_json/tt" + id + ".json");
+            logger.debug("Found reviews:  " + fileAsString);
+            AmazonReview[] amazonReviews = objectMapper.readValue(fileAsString, AmazonReview[].class);
+            Arrays.stream(amazonReviews).map(r -> new Review(r.getStar_rating(), r.getReview_title(), r.getReview_text())).collect(toList());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
         List<Person> people = new ArrayList();
         return new MovieDetail(UUID.nameUUIDFromBytes(id.getBytes()),
-                "Fake Name",
+                null,
                 people,
                 asList(),
-                Faker.instance().lorem().paragraph(),
-                Faker.instance().lorem().words(3),
+                "",
+                asList(),
                 LocalDate.now(),
-                "Fake",
-                "url",
+                "",
+                "",
                 reviews,
                 this.getLineage());
     }
+
 }
--- ui/server/src/test/java/group/u/records/security/AWSCloudHSMSecurityGatewayClientTest.java
+++ ui/server/src/test/java/group/u/records/security/AWSCloudHSMSecurityGatewayClientTest.java
@@ -23,4 +23,20 @@ public class AWSCloudHSMSecurityGatewayClientTest {
 
         assertThat(content).isEqualTo(client.decrypt(id, encrypt));
     }
+
+    @Test
+    public void shouldFailBasedOnCurrentBug(){
+        AWSCloudHSMSecurityGatewayClient client = new AWSCloudHSMSecurityGatewayClient(new RestTemplate(), "http://13.52.124.47:8080" );
+        String content = "this is a random text string";
+        UUID id = UUID.randomUUID();
+        String encrypt = client.encrypt(id, content);
+        String secondEncrypt = client.encrypt(id, content);
+
+        assertThat(encrypt).isEqualTo(secondEncrypt);
+        System.out.println(encrypt);
+        assertThat(encrypt).isNotNull();
+        assertThat(content).isEqualTo(client.decrypt(id, secondEncrypt));
+        assertThat(content).isEqualTo(client.decrypt(id, encrypt));
+
+    }
 }
