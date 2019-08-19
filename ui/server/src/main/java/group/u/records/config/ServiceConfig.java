87b0819f8b815a6e1ae6a7efb98c7a2901ee8f82
Carlyle S. Davis <carlyle.davis@bytecubed.com>
2019-08-18 20:37:13 -0400

    UR-21 - @carlyledavis:  Adding the ability to add Amazon data sources.

--- ui/server/src/main/java/group/u/records/config/ServiceConfig.java
+++ ui/server/src/main/java/group/u/records/config/ServiceConfig.java
@@ -53,16 +53,16 @@ public class ServiceConfig {
         return new RestTemplate();
     }
 
-    @Bean
-    @ConditionalOnProperty(value = "${app.security.hsm.enabled:true}", matchIfMissing = true)
-    public SecurityGatewayClient securityGatewayClient(RestTemplate restTemplate,
-                                                       @Value("${app.content.security.host}") String host){
-        logger.debug("Initializing in HSM Gateway client." );
-        return new AWSCloudHSMSecurityGatewayClient(restTemplate, host );
-    }
+//    @Bean
+//    @ConditionalOnProperty(value = "${app.security.hsm.enabled}", havingValue = "true", matchIfMissing = true)
+//    public SecurityGatewayClient securityGatewayClient(RestTemplate restTemplate,
+//                                                       @Value("${app.content.security.host}") String host){
+//        logger.debug("Initializing in HSM Gateway client." );
+//        return new AWSCloudHSMSecurityGatewayClient(restTemplate, host );
+//    }
 
     @Bean
-    @ConditionalOnProperty(value = "${app.security.hsm.enabled:false}")
+//    @ConditionalOnProperty(value = "${app.security.hsm.enabled}", havingValue = "false")
     public SecurityGatewayClient inMemorySecurityGatewayClient(){
         logger.debug("Initializing in memory security client." );
         return new InMemorySecurityClient();
@@ -72,7 +72,7 @@ public class ServiceConfig {
     public MovieDetailsDataSourceManager dataSourceManager(ImdbMovieDetailsDataSource imdb,
                                                            OmdbMovieDetailsDataSource omdb,
                                                            AmazonReviewsDataSource amazon ){
-        return new MovieDetailsDataSourceManager(asList(imdb));
+        return new MovieDetailsDataSourceManager(asList(imdb, amazon));
     }
 
 }
--- ui/server/src/main/java/group/u/records/models/entity/Review.java
+++ ui/server/src/main/java/group/u/records/models/entity/Review.java
@@ -1,31 +1,30 @@
 package group.u.records.models.entity;
 
-import java.time.LocalDate;
-
 public class Review {
 
-    private String reviewer;
-    private LocalDate timeStamp;
+    public String getRating() {
+        return rating;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    private String rating;
+    private String title;
     private String content;
 
     public Review() {
     }
 
-    public Review(String reviewer, LocalDate timeStamp, String content) {
-        this.reviewer = reviewer;
-        this.timeStamp = timeStamp;
+    public Review(String rating, String title, String content) {
+        this.rating = rating;
+        this.title = title;
         this.content = content;
-    }
-
-    public String getReviewer() {
-        return reviewer;
-    }
-
-    public LocalDate getTimeStamp() {
-        return timeStamp;
     }
 
     public String getContent() {
         return content;
     }
+
 }
