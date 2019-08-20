fdcbfefae436c91a4776b38d8d9e7f97e5c36d53
Carlyle S. Davis <carlyle.davis@bytecubed.com>
2019-08-20 13:53:10 -0400

    UR-25 - @carlyledavis:  Updating terraform configuration for better support on the cloud.

--- config/hsm-setup/initial-setup/hsm.tf
+++ config/hsm-setup/initial-setup/hsm.tf
@@ -183,8 +183,9 @@ resource aws_instance hsm_gateway {
     inline = [
       "chmod +x /tmp/setup_ec2.sh",
       "chmod +x /tmp/expect_script.sh",
-      "/tmp/setup_ec2.sh ${aws_cloudhsm_v2_hsm.cloudhsm_v2_hsm.hsm_id} ${aws_iam_user_login_profile.admin.encrypted_password} ${aws_cloudhsm_v2_hsm.cloudhsm_v2_hsm.ip_address}"],
+      "/tmp/setup_ec2.sh ${aws_cloudhsm_v2_hsm.cloudhsm_v2_hsm.hsm_id} ${aws_iam_user_login_profile.admin.encrypted_password} ${aws_cloudhsm_v2_hsm.cloudhsm_v2_hsm.ip_address}",
       "java -Djava.library.path=/opt/cloudhsm/lib -jar /tmp/hsmgateway-0.0.1-SNAPSHOT.jar"
+    ]
   }
 
   depends_on = [aws_cloudhsm_v2_hsm.cloudhsm_v2_hsm, aws_iam_user_login_profile.admin, aws_security_group.gateway-ingress, tls_private_key.hsm_key]
