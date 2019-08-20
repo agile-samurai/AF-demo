2f3ef49255daa435a8d6a241d1d2fcdb9318a115
Carlyle S. Davis <carlyle.davis@bytecubed.com>
2019-08-20 01:36:43 -0400

    UR-23 - @carlyledavis - cloud hsm in pipeline.

--- config/hsm-setup/initial-setup/main.tf
+++ config/hsm-setup/initial-setup/main.tf
@@ -1,8 +1,3 @@
-variable region {
-  type    = "string"
-  default = "us-west-1"
-}
-
 terraform {
   backend "s3" {
     bucket = "rdso-challenge2"
@@ -15,13 +10,26 @@ provider "aws" {
   version = "~> 2.0"
 }
 
+variable "aws_region" {
+  type        = map(string)
+
+  default = {
+    #default = "us-east-1",
+    dev  = "us-east-1",
+    test = "us-east-2",
+    prod = "us-west-1",
+    concourse   = "us-west-2",
+    infra   = "us-west-2"
+  }
+}
+
 module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
   name = "hsm-vpc"
   cidr = "10.6.0.0/16"
 
-  azs = ["${var.region}a"]
+  azs = ["${var.aws_region["${terraform.workspace}"]}a"]
   private_subnets = ["10.6.1.0/24"]
   public_subnets = ["10.6.101.0/24"]
 
