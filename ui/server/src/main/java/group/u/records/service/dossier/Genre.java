438556b27427fd849f4e25765b49afe9cf6cd460
Carlyle S. Davis <carlyle.davis@bytecubed.com>
2019-08-19 20:30:42 -0400

    UR-24 - Updating Master Dossier document needs to hold distribution images.

--- ui/server/src/main/java/group/u/records/service/dossier/Genre.java
+++ ui/server/src/main/java/group/u/records/service/dossier/Genre.java
@@ -2,20 +2,14 @@ package group.u.records.service.dossier;
 
 public class Genre {
     private String genre;
-    private String img;
 
     public String getGenre() {
         return genre;
     }
 
-    public String getImg() {
-        return img;
-    }
-
     public Genre(){}
 
-    public Genre(String genre, String img) {
+    public Genre(String genre) {
         this.genre = genre;
-        this.img = img;
     }
 }
