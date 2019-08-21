6bf8d91dfac7024637d6f9ce161d07bb8c0aad5b
bradculley <bradley.culley@bytecubed.com>
2019-08-21 15:30:08 -0400

    UR-1 - Training data paginated on UI

--- ui/frontend/src/components/TrainingData/TrainingData.js
+++ ui/frontend/src/components/TrainingData/TrainingData.js
@@ -1,11 +1,57 @@
 import React from 'react';
+import axios from "axios/index";
 
 export default class TrainingData extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            trainingData: [],
+            cursor: 0
+        };
+        this.TRAINING_ENDPOINT = '/api/training';
+
+        this.setUpInfiniteScroll();
+    }
+
+    componentDidMount() {
+        this.loadMore();
+    }
+
     render() {
+        const processedTrainingData = this.state.trainingData.map(data => {
+            const splitElement = data.key.split(': ')[1].trim();
+            return <div><br/><a key={data.key} href={`https://ugroup-rdso-challenge-data.s3.amazonaws.com/${splitElement}`}>
+                {`https://ugroup-rdso-challenge-data.s3.amazonaws.com/${splitElement}`}</a><br/></div>;
+        });
+
         return (
             <div>
-                Stub of training data page
+                {processedTrainingData}
             </div>
         );
+    }
+
+    setUpInfiniteScroll() {
+        window.onscroll = () => {
+            if ((window.innerHeight + window.scrollY) >= (document.body.offsetHeight - 300)) {
+                this.loadMore();
+            }
+        };
+    }
+
+    loadMore() {
+        const {cursor} = this.state;
+
+        axios.get(this.TRAINING_ENDPOINT, {
+            params: {
+                cursor: cursor + 1
+            }
+        })
+            .then(response => {
+                this.setState({
+                    trainingData: this.state.trainingData.concat(response.data.content),
+                    cursor: cursor + 1
+                });
+            });
     }
 }
