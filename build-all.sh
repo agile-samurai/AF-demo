4a93ef4dbbe7c1e0227cf5c3bf2a7e56ecdd814e
bradculley <bradley.culley@bytecubed.com>
2019-08-21 10:00:27 -0400

    UR-1 - Refactoring to update name for FE project

--- build-all.sh
+++ build-all.sh
@@ -1,5 +1,5 @@
 PROJECT_ROOT=$(pwd)
-UI_SERVER_FRONTEND=$PROJECT_ROOT/sector-ui
+UI_SERVER_FRONTEND=$PROJECT_ROOT/frontend
 UI_SERVER_BACKEND=$PROJECT_ROOT/server
 DATA_SCIENCE_ROOT=$PROJECT_ROOT/services/data-science-service
 
@@ -7,7 +7,7 @@ DATA_SCIENCE_ROOT=$PROJECT_ROOT/services/data-science-service
 docker rmi records/ui-backend || true && cd ui/server && mvn clean install -Pdocker -DskipTests
 
 # Build UI client
-cd $UI_SERVER_FRONTEND/container && ./build-sector-ui.sh
+cd $UI_SERVER_FRONTEND/container && ./build-frontend.sh
 
 # Build data science service (REST-based application)
 cd $PROJECT_ROOT && ./build-ds.sh
--- ui/sector-ui/container/build-sector-ui.sh
+++
@@ -1,7 +0,0 @@
-docker rmi records/sector-ui || true
-cd ../
-npm install
-npm run build
-cd ./container
-cp -Rf ../build .
-docker build  . -t records/sector-ui:latest
