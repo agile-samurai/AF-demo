e3aee189a5c6db72da650a26c3326658767885d2
bradculley <bradley.culley@bytecubed.com>
2019-08-21 06:05:28 -0400

    UR-1 - Further infra work for local env

--- build-all.sh
+++ build-all.sh
@@ -1,7 +1,17 @@
-./tools/prepare-ds-local.sh
-mvn -f ./ui/server/pom.xml clean install -DskipTests -Pdocker
-cd ui/sector-ui/
-npm run build
-cd container
-./build-sector-ui.sh
-cd ../../
+PROJECT_ROOT=$(pwd)
+UI_SERVER_FRONTEND=$PROJECT_ROOT/sector-ui
+UI_SERVER_BACKEND=$PROJECT_ROOT/server
+DATA_SCIENCE_ROOT=$PROJECT_ROOT/services/data-science-service
+
+# Build UI server
+docker rmi records/ui-backend || true && cd ui/server && mvn clean install -Pdocker -DskipTests
+
+# Build UI client
+cd $UI_SERVER_FRONTEND/container && ./build-sector-ui.sh
+
+# Build data science service (REST-based application)
+cd $PROJECT_ROOT && ./build-ds.sh
+
+# Build data science notebook
+cd $DATA_SCIENCE_ROOT && docker rmi ugroup/data-science-notebook || true
+docker build -t ugroup/data-science-notebook . --build-arg AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID --build-arg AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY
