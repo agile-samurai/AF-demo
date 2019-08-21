9dd1e23b5d37e5d183b5538d8621a1604e491461
Ian Williams <ianwilliams@bytecubed.com>
2019-08-21 13:09:51 -0400

    remove backend from hsm

--- config/hsm-setup/initial-setup/main.tf
+++ config/hsm-setup/initial-setup/main.tf
@@ -1,10 +1,10 @@
-terraform {
-  backend "s3" {
-    #bucket = "infra-735978838162"
-    #key    = "hsm.tfstate"
-    #region = "us-east-1"
-  }
-}
+# terraform {
+#   backend "s3" {
+#     #bucket = "infra-735978838162"
+#     #key    = "hsm.tfstate"
+#     #region = "us-east-1"
+#   }
+# }
 
 terraform {
   required_version = "0.12.5"
