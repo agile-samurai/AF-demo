ab42c5bdc39e2acf566ac18db458832d7861a840
bradculley <bradley.culley@bytecubed.com>
2019-08-21 03:18:15 -0400

    UR-1 - Making build-ds.sh point at public models and pkl file

--- build-ds.sh
+++ build-ds.sh
@@ -4,11 +4,11 @@ cp ../../services/data-science-service/rdso/* .
 mkdir models
 mkdir data
 cd models
-aws s3 cp s3://rdso-challenge2/data/ci/models/metrics.0.0.0.json .
-aws s3 cp s3://rdso-challenge2/models/movies_doc2vec.0.0.0.model .
-aws s3 cp s3://rdso-challenge2/models/ . --recursive
+aws s3 cp s3://ugroup-rdso-challenge-data/models/metrics.0.0.0.json .
+aws s3 cp s3://ugroup-rdso-challenge-data/models/movies_doc2vec.0.0.0.model .
+aws s3 cp s3://ugroup-rdso-challenge-data/models/ . --recursive
 
 cd ../data
-aws s3 cp s3://rdso-challenge2/data/ci/data/movies_df.pkl .
+aws s3 cp s3://ugroup-rdso-challenge-data/data/movies_df.pkl .
 cd ..
 docker build . -t ugroup/data-science-service
